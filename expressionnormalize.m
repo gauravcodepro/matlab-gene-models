89a36ce241a4800c09dcabf1c13a5ccaa35b94f7
Gaurav <gaurav.sablok@uni-potsdam.de>
2024-05-17 12:03:53 +0200

    expression normalization matlab

---
+++ expressionnormalize.m
@@ -0,0 +1,8 @@
+# Author Gaurav
+# Universitat Potsdam
+# Date 2024-5-17 
+/*
+ a matlab function for the normalization of the gene expression levels for the 
+ estimation of the upper bound and the lower bound levels. 
+*/
+ readfile 
