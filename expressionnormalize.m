bd73dfca4db547f4630da399b976e67714e042a7
Gaurav <gaurav.sablok@uni-potsdam.de>
2024-05-17 14:12:35 +0200

    completing the matlab code 2024-5-17

--- expressionnormalize.m
+++ expressionnormalize.m
@@ -4,5 +4,10 @@
 /*
  a matlab function for the normalization of the gene expression levels for the 
  estimation of the upper bound and the lower bound levels. 
+ reads a gene expression file and then log normalizes it and then estimated the gpr rules
+ implemented the readtable instead of the readxls for faster reading of the files.
 */
- readfile 
+ readfile = input(prompt, "please provide the gene expression file")
+ sheet = input(prompt, "please provide the sheet number")
+ fileread = readtable(readfile, "sheet")
+ 
